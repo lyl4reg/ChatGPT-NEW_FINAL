7ff36b693cf5180684554360f9d9c4cc4ff39072
lvguanjun <lvguanjun135@foxmail.com>
2024-01-20 15:18:21 +0800

    Add reflush prompt
    Reference:
    https://chat.caifree.com/

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -158,7 +158,7 @@ export function SideBar(props: { className?: string }) {
           NextChat
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          返回出现乱码，请刷新网页后重试请求.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
