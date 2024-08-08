cc6d57baad3392a6d1468b5baa88ad2315044e55
lyl4reg <lyl4reg@gmail.com>
2024-08-08 23:00:44 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,10 +10,10 @@ import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "ChatGpt(private version)",
+  description: "自用-请勿外传",
   appleWebApp: {
-    title: "NextChat",
+    title: "文书批改",
     statusBarStyle: "default",
   },
 };
